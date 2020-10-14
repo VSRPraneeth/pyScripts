154d56aba8cf4867208ad7b411478682016730e3
Praneeth <vprnth9@gmail.com>
2020-10-14 18:40:48 -0400

    transfer

--- first/dictionary.py
+++ first/dictionary.py
@@ -1,9 +1,9 @@
-classmates = {'Tony' : ' Cool but smells', 'Emma' : ' Sits beside me', 'Lucy' : ' She is hot', 'Lucy' : ' She is cool'}
+classmates = {'Tony' : 'Cool but smells', 'Emma' : 'Sits beside me', 'Lucy' : 'She is hot', 'Lucy' : 'She is cool'}
 
 print(classmates)
 print(classmates['Emma'])
 print(classmates['Tony'])
 print(classmates['Lucy'])
 
-for k,v in classmates.items():
+for k, v in classmates.items():
     print(k)
